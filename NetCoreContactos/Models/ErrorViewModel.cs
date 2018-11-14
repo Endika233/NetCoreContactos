1a19bf917a756f4fbb50592ddf85fb712a8d5ef8
Endika233 <43742370+Endika233@users.noreply.github.com>
2018-11-14 11:55:01 +0100

    Agregar archivos de proyecto.

---
+++ NetCoreContactos/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace NetCoreContactos.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
